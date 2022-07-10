14dedab7f55304c5738c674f0296cccc59d5e1aa
Nart Aliti <76398544+alitinart@users.noreply.github.com>
2022-07-10 22:27:36 +0200

    Custom JpaRepository Methods

--- src/main/java/com/alitinart/BlogAPI/post/PostRepository.java
+++ src/main/java/com/alitinart/BlogAPI/post/PostRepository.java
@@ -1,9 +1,14 @@
 package com.alitinart.BlogAPI.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    @Query("SELECT s FROM Post s WHERE s.title = ?1")
+    Optional<Post> findPostByTitle(String title);
 }
